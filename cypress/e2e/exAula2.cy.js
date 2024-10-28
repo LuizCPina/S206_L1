f2113280b36921aa15d5c628630cf86182733f94
LuizCPina <luizricopina@hotmail.com>
2024-10-28 16:31:21 -0300

    cod1

---
+++ cypress/e2e/exAula2.cy.js
@@ -0,0 +1,54 @@
+/// <reference = cypress>
+
+describe("Testes de criação, registro e login", ()=>{
+    it("Teste criação de usuario com sucesso", ()=>{
+        let info = criarUser()
+        cy.visit('https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('.btn-link').click()
+        cy.get('#firstName').type(info[0])
+        cy.get('#Text1').type(info[0])
+        cy.get('#username').type(info[0])
+        cy.get('#password').type(info[1])
+       
+        
+    })
+
+    it.skip("Teste de criação de ususario com falha",()=>{
+        cy.visit('https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('.btn-link').click()
+        cy.get('#firstName').type("luiz")
+        cy.get('#Text1').type("luiz")
+        cy.get('#username').type("luiz")
+        cy.get('.form-actions').should("be.disabled")
+        
+    })
+
+    it("Teste de login com sucesso",()=>{
+        let info = criarUser()
+        cy.visit('https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('#username').type(info[0])
+        cy.get('#password').type(info[1])
+
+
+    })
+    })
+
+    function criarUser(){
+        let hora=new Date().getHours().toString()
+        let minuto = new Date().getMinutes().toString()
+        let seg = new Date().getSeconds().toString()
+        let id = hora+minuto+seg+ "id"
+        let senha = hora+minuto+seg+"senha"
+        let info = [id,senha]
+
+        cy.visit('https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('.btn-link').click()
+        cy.get('#firstName').type(id)
+        cy.get('#Text1').type(id)
+        cy.get('#username').type(id)
+        cy.get('#password').type(senha)
+        cy.get('.form-actions').click()
+        cy.get('.ng-binding').should("contain.text","Registration successful")
+
+        return info
+    }
