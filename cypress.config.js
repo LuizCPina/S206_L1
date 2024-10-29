13d340bf2c19cd8e75284b1c71c4b7bf2417c11c
LuizCPina <luizricopina@hotmail.com>
2024-10-28 21:12:36 -0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
-      //
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
